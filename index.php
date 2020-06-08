08e0516560ccb6e880d637b5ce04ba14e5ccea31
Guilherme-j10 <guilhermeZA.123123@gmail.com>
2020-06-07 21:58:03 -0300

    tratamento de erro adicionado

--- index.php
+++ index.php
@@ -11,9 +11,11 @@
 
     require_once "vendor/autoload.php";
 
-    use src\Express;
+    use src\Express as App;
 
-    $app = new Express();
+    $app = new App();
+
+    $app->type_aplication('api');
 
     $users = [
         "Guilherme" => 'campos',
@@ -24,6 +26,10 @@
 
     $app->get('/', function($request, $response) {
         $response['redirect']('recebe_redirect');
+    });
+
+    $app->get('/contatos/empresa', function($request, $response) {
+        $response['send']('composto');
     });
 
     $app->get('/recebe_redirect', function($request, $response) {
@@ -39,9 +45,31 @@
         $response['json']($data);
     });
 
+    //usar o curl para fazer o envio dos dados
     $app->post('/postagem', function($request, $response) use($users) {
         $users[$request['body']['nome']] = $request['body']['sobrenome'];
         $response['json']($users);
     });
+
+    $app->put('/users', function($request, $response) use($users){
+        $users[$request['body']['nome']] = $request['body']['sobrenome'];
+
+        $response['json']($users);
+    });
+
+    $app->delete('/users', function($request, $response) use($users) {
+        unset($users[$request['body']['nome']]);
+
+        $response['json']($users);
+    });
+
+    $app->error($_GET['aplication'], function($response) {
+        $response['send']('
+            <h1>Página nao encontrada</h1>
+            <a href="listagem">voltar para a listagem</a>
+        ');
+    });
+
+    
     
   
--- src/Express.php
+++ src/Express.php
@@ -3,20 +3,24 @@
     namespace src;
     error_reporting(0);
 
-    use src\TreatmentRoute;
+    use src\TreatmentRoute as Router;
 
-    class Express extends TreatmentRoute
+    class Express extends Router
     {
 
         protected $method_request;
         protected $route_aplication_separete;
         protected $simple_route_aplication;
+        protected $get_request;
 
         protected $response_params = [];
         protected $request_params = [];
+        protected $all_routes = [];
 
         public function __construct()
-        {
+        {   
+            $this->get_request = $_GET['aplication'];
+
             $this->method_request = $_SERVER["REQUEST_METHOD"];
             $this->simple_route_aplication = $_GET["aplication"] ?? '/';
             $this->route_aplication_separete = explode('/', $this->simple_route_aplication);
@@ -36,40 +40,64 @@
 
         public function get($route, $func)
         { 
-            $this->params($route, $_GET["aplication"]); //tem que vir antes do route ser reescrevido pelo método Route
+            $this->params($route, $this->get_request); //tem que vir antes do route ser reescrevido pelo método Route
 
-            $route = $this->Route($route, $_GET["aplication"]); 
+            $route = $this->Route($route, $this->get_request); 
 
             if($this->method_request == 'GET' AND $route == $this->simple_route_aplication){
+                $this->all_routes[] = $route;
+
                 $func($this->request_params, $this->response_params);
+            }else{
+                $this->all_routes[] = $route;
             }
         }
 
         public function post($route, $func)
         { 
-            $this->params($route, $_GET["aplication"]);
+            $this->params($route, $this->get_request);
 
-            $route = $this->Route($route, $_GET["aplication"]);
+            $route = $this->Route($route, $this->get_request);
 
             if($this->method_request == 'POST' AND $route == $this->simple_route_aplication){
-                $body_request = file_get_contents("php://input");
-                $this->request_params["body"] = json_decode($body_request, true);
+                $this->request_body();
+                $this->all_routes[] = $route;
                 
                 $func($this->request_params, $this->response_params);
+            }else{
+                $this->all_routes[] = $route;
             }
         }
 
-        public function put()
+        public function put($route, $func)
         {
-            if($this->method_request == 'PUT'){
-                return json_encode('PUT');
+            $this->params($route, $this->get_request);
+
+            $route = $this->Route($route, $this->get_request);
+
+            if($this->method_request == 'PUT' AND $route == $this->simple_route_aplication){
+                $this->request_body();
+                $this->all_routes[] = $route;
+
+                $func($this->request_params, $this->response_params);
+            }else{
+                $this->all_routes[] = $route;
             }
         }
 
-        public function delete()
+        public function delete($route, $func)
         {
-            if($this->method_request == 'DELETE'){
-                return json_encode('DELETE');
+            $this->params($route, $this->get_request);
+
+            $route = $this->Route($route, $this->get_request);
+
+            if($this->method_request == 'DELETE' AND $route == $this->simple_route_aplication){
+                $this->request_body();
+                $this->all_routes[] = $route;
+
+                $func($this->request_params, $this->response_params);
+            }else{
+                $this->all_routes[] = $route;
             }
         }
 
@@ -77,5 +105,38 @@
         {
             $this->request_params["params"] = $this->treat_param($route, $request); 
             $this->request_params["params"] = $this->takeof_doubleDotos($this->request_params["params"]);
+        }
+
+        public function request_body(){
+            $body_request = file_get_contents("php://input");
+            $this->request_params["body"] = json_decode($body_request, true);
+        }
+
+        public function type_aplication($type){
+            if($type == 'web' OR $type == 'api'){
+                return $type;
+            }else{
+                echo 'type aplication invalid, web or api';
+            }
+        }
+
+        public function verify_route($route_request)
+        {
+            $valor = '';
+
+            foreach ($this->all_routes as $key => $value) {
+                if($route_request == $this->all_routes[$key]){
+                    $valor = $this->all_routes[$key];
+                }
+            }
+
+            return $valor;
+        }
+
+        public function error($route_request, $func)
+        {
+            if($route_request !== $this->verify_route($route_request)){
+                $func($this->response_params);
+            }
         }
     }
